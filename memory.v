8deb73bf31a111bc77009705fd8525a0f6893d3b
Paul <paulb2233@yahoo.com>
2020-12-01 23:06:46 +0200

    adaugarea memoriei pentru stocarea datelor

--- memory.v
+++ memory.v
@@ -1,6 +1,7 @@
 `include "temaCISL.v"
 
-module sram_1port(
+//sram used for instructions
+module sram_1port_instructions(
     input clk,
     input [15:0] address,
     input wr,rd,
@@ -16,6 +17,25 @@ always @ (posedge clk) begin
 end
 
 endmodule
+
+//sram used for data
+module sram_1port_data(
+    input clk,
+    input [15:0] address,
+    input wr, rd,
+    input [12:0] wr_data,
+    output reg [12:0] rd_data
+);
+
+reg [12:0] mem_reg [65535:0];
+
+always @ (posedge clk) begin
+    if(wr) mem_reg[address] <= wr_data;
+    else if(rd) rd_data <= mem_reg[address];
+end
+
+endmodule
+
 //automaton
 module control_sram(
     input clk, wr, rd, rst,
@@ -23,16 +43,20 @@ module control_sram(
     output [12:0] out
 );
 
-reg [15:0] address,address_rd,address_wr;
+reg [15:0] address,address_rd,address_wr,adr;
+reg wr_en,rd_en;
 wire [2:0] rd_data;
 
 initial address = 16'd0;
 initial address_wr = 16'd0;
 initial address_rd = 16'd0;
+initial adr = 16'd0;
 
-sram_1port i0(.clk(clk),.address(address),.wr(wr),
+sram_1port_instructions i0(.clk(clk),.address(address),.wr(wr),
 .rd(rd),.wr_data(wr_data),.rd_data(rd_data));
-control_unit i1(.clk(clk),.rst(rst),.in(rd_data),.o(out));
+sram_1port_data i1(.clk(clk),.address(adr),.wr(wr_en),.rd(rd_en),
+.wr_data(out));//missing rd_data
+control_unit i2(.clk(clk),.rst(rst),.in(rd_data),.o(out));
 
 always @(posedge clk) begin
     if(wr) begin
@@ -44,6 +68,11 @@ always @(posedge clk) begin
         address_rd = address_rd + 1;
         address = address_rd;
         address_wr = 16'd0;
+        if(address_rd == 3'b010) begin
+            adr = adr + 1;
+            wr_en = 1'b1;
+            rd_en = 1'b0;
+        end 
     end
 end
 
